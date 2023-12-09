885290ac238bea835534e046c0bc1803c6ef52a9
Noah Lett <noahblett96@gmail.com>
2023-12-09 12:36:38 -0800

    Created initial Card component and incorporated into home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import prisma from '@/lib/prisma'
+import Card from './components/Card';
 
 async function getRecipes() {
   const recipes = await prisma.recipe.findMany({
@@ -14,10 +15,22 @@ async function getRecipes() {
 
 export default async function Home() {
   const recipes = await getRecipes();
-  console.log(recipes);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Let&apos;s Get Cooking!</h1>
+      {
+        recipes.map((recipe) => {
+          return (
+            <Card
+              key={recipe.id}
+              id={recipe.id}
+              name={recipe.name}
+              image_src={recipe.image_src}
+              author={recipe.author.name} 
+            />
+          )
+        })
+      }
     </main>
   )
 }
