405d21f62fea13bdc56b45f83a7fa38d1d8b4036
Noah Lett <noahblett96@gmail.com>
2024-01-15 16:46:56 -0800

    Made some visual improvements to shopping list page

--- app/components/ShoppingList.jsx
+++ app/components/ShoppingList.jsx
@@ -4,17 +4,22 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import ShoppingListItem from './ShoppingListItem';
 import dynamic from 'next/dynamic';
+import { FaBasketShopping } from "react-icons/fa6";
+
 
 const IngredientList = () => {
   const shoppingListObj = useSelector(state => state.shoppingList.shoppingList);
 
   return (
     <div>
-      <ul>
+      <ul className='bg-slate-100 rounded-md mt-5'>
         {shoppingListObj && shoppingListObj.length > 0 ? shoppingListObj.map((ingredient, index) => (
           <ShoppingListItem key={index} ingredient={ingredient} />
         )):
-          <span>No Ingredients</span>
+        <div className='flex flex-col items-center mt-16'>
+          <h3 className='text-2xl font-medium text-center'>Ghost town vibes 👻<br/> Your list is empty!</h3>
+          <FaBasketShopping className='text-7xl text-slate-100 mt-5'></FaBasketShopping>
+        </div>
         }
       </ul>
     </div>
