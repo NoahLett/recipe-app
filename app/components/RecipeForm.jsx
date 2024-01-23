719d5249ffeb4c709279702a096c6aee1dcb63f5
Noah Lett <noahblett96@gmail.com>
2024-01-23 08:45:10 -0800

    Made some improvements to the CSS of submission form

--- app/components/RecipeForm.jsx
+++ app/components/RecipeForm.jsx
@@ -1,6 +1,8 @@
 'use client'
 
 import React, { useState } from 'react';
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoAddCircleOutline } from "react-icons/io5";
 
 const RecipeForm = () => {
 
@@ -72,55 +74,64 @@ const RecipeForm = () => {
   };
 
   return (
-    <div className='min-w-[300px] max-w-screen-sm mx-auto p-2'>
-      <form className='rounded-md bg-white p-3' onSubmit={handleSubmit}>
-        <label>
-          Name:
-          </label><br/>
-          <input
-            type="text"
-            value={formData.recipeName}
-            onChange={(e) => handleInputChange(e, null, 'name')}
-            className='border-solid border-2 border-slate-400 rounded'
-          />
-
+    <div className='min-w-[300px] max-w-screen-sm mx-auto p-2 pt-4'>
+      <form className='rounded-md bg-white p-4' onSubmit={handleSubmit}>
         <div>
-          <label>Ingredients:</label>
+          <label className='text-lg font-medium'>Recipe Name</label>
+            <div className='my-2'>
+              <input
+                type="text"
+                value={formData.recipeName}
+                onChange={(e) => handleInputChange(e, null, 'name')}
+                className='border-solid border-2 border-slate-400 rounded min-w-[342.8px] h-8'
+              />
+            </div>
+          </div>
+        <div className='flex flex-col'>
+          <label className='text-lg font-medium'>Ingredients</label>
           {formData.ingredients.map((ingredient, index) => (
-            <div key={index}>
+            <div className='my-2 flex' key={index}>
               <input
                 type="text"
                 value={ingredient}
                 onChange={(e) => handleInputChange(e, index, 'ingredients')}
-                className='border-solid border-2 border-slate-400 rounded'
+                className='border-solid border-2 border-slate-400 rounded w-11/12 h-8 p-1'
               />
               {index > 0 && (
-                <button type="button" onClick={() => handleRemoveInput(index, 'ingredients')}>
-                  Remove
+                <button 
+                  type="button" 
+                  onClick={() => handleRemoveInput(index, 'ingredients')}
+                  className='ml-2 hover:text-red-400 transition-all duration-150'
+                  >
+                  <FaRegTrashCan className='text-xl' />
                 </button>
               )}
             </div>
           ))}
-          <button type="button" onClick={() => handleAddInput('ingredients')}>
-            Add Ingredient
+          <button 
+            type="button" 
+            onClick={() => handleAddInput('ingredients')}
+            className='self-center'>
+            <IoAddCircleOutline className='text-2xl hover:text-sky-400 transition-all duration-150' />
           </button>
         </div>
 
-        <div>
-          <label>Steps:</label>
+        <div className='flex flex-col'>
+          <label className='text-lg font-medium'>Steps</label>
           {formData.steps.map((step, index) => (
-            <div key={index}>
+            <div className='flex my-2' key={index}>
               <input
                 type="text"
                 value={step}
                 onChange={(e) => handleInputChange(e, index, 'steps')}
-                className='border-solid border-2 border-slate-400 rounded '
+                className='border-solid border-2 border-slate-400 rounded w-11/12 h-8 p-1'
               />
               {index > 0 && (
                 <button
                  type="button" 
-                 onClick={() => handleRemoveInput(index, 'steps')}>
-                  Remove
+                 onClick={() => handleRemoveInput(index, 'steps')}
+                 className='ml-2 hover:text-red-400 transition-all duration-150'>
+                  <FaRegTrashCan className='text-xl' />
                 </button>
               )}
             </div>
@@ -128,16 +139,18 @@ const RecipeForm = () => {
           <button 
             type="button" 
             onClick={() => handleAddInput('steps')}
-            className='pb-2'>
-            Add Step
+            className='pb-2 self-center'>
+            <IoAddCircleOutline className='text-2xl hover:text-sky-400 transition-all duration-150' />
           </button>
         </div>
-
-        <button 
-          type="submit"
-          className='border-solid border-[1px] border-black rounded hover:bg-green-400 hover:text-white transition-all duration-150 p-1'>
-            Submit
-        </button>
+        
+        <div className='flex justify-end'>
+          <button 
+            type="submit"
+            className='border-solid border-[1px] border-black rounded hover:bg-green-400 hover:text-white hover:border-green-400 transition-all duration-150 p-1'>
+              Submit
+          </button>
+        </div>
       </form>
     </div>
   );
