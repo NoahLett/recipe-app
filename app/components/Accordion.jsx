e895a0a2e5f22bd611729348f5615b7746ec25c3
Noah Lett <noahblett96@gmail.com>
2024-01-24 12:08:33 -0800

    Added actual text to accordion data array

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -13,16 +13,16 @@ const Accordion = ({ items }) => {
   };
 
   return (
-    <div className="w-full">
+    <div>
       {items.map((item, index) => (
         <div key={index}>
           <div
-            className={`p-4 cursor-pointer ${index !== items.length - 1 ? 'border-b' : ''}`}
+            className={`p-4 cursor-pointer ${index !== items.length - 1 ? 'border-b border-slate-300' : ''}`}
             onClick={() => handleItemClick(index)}
           >
             <div className="flex justify-between items-center text-lg">
-                <span>{item.title}</span>
-                {activeIndex === index ? <FaChevronDown /> : <FaChevronUp />}
+                <div>{item.title}</div>
+                <div>{activeIndex === index ? <FaChevronDown className="text-lg" /> : <FaChevronUp className="text-lg" />}</div>
             </div>
           </div>
           {activeIndex === index && (
