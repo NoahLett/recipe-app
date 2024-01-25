09168ff2bf2dc09b2c789df006b12180b8887804
Noah Lett <noahblett96@gmail.com>
2024-01-25 08:51:50 -0800

    Ended up making a hero section instead

---
+++ app/components/Hero.jsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+
+const Hero = () => {
+
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div className='w-full bg-white py-7 md:py-20'>
+      <div className='max-w-[100%] mx-auto grid grid-cols-2'>
+        <div className='flex flex-col justify-center ml-[2rem] md:ml-[7rem]'>
+          <p className='text-md md:text-2xl text-[#6CA7A9] font-medium my-1'>EXQUISITE RECIPES</p>
+          <h1 className='text-2xl md:text-5xl font-semibold'>Hey there, {session?.user?.name}!</h1>
+          <p className='text-md md:text-xl text-slate-500 font-medium my-2'>Let's Get That List Going!</p>
+        </div>
+        <Image src={'/chef.svg'} height={300} width={500} priority={true} alt='hero image' />
+      </div>
+    </div>
+    )
+  } else {
+    return (
+      <div className='w-full bg-white py-7 md:py-20'>
+      <div className='max-w-[100%] mx-auto grid grid-cols-2'>
+        <div className='flex flex-col justify-center ml-[2rem] md:ml-[7rem]'>
+          <p className='text-md md:text-2xl text-[#6CA7A9] font-medium my-1'>EXQUISITE RECIPES</p>
+          <h1 className='text-2xl md:text-5xl font-semibold'>Your Custom Shopping List Is A Tap Away</h1>
+          <p className='text-md md:text-xl text-slate-500 font-medium my-2'>Save Some Time. Get Browsin&apos;!</p>
+        </div>
+        <Image src={'/chef.svg'} height={300} width={500} priority={true} alt='hero image' />
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Hero
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 import Card from './components/Card';
 import getRecipes from '@/lib/getRecipes';
+import Hero from './components/Hero';
 
 export default async function Home() {
   const recipes = await getRecipes();
   return (
-    <main className="flex flex-wrap items-center justify-center min-h-screen pt-20">
+    <main className="flex flex-col items-center justify-center min-h-screen pt-[3.5rem]">
+      <Hero />
+      <div className='flex flex-wrap items-center justify-center'>
       {
         recipes.map((recipe) => {
           return (
@@ -19,6 +22,7 @@ export default async function Home() {
           )
         })
       }
+      </div>
     </main>
   )
 }
