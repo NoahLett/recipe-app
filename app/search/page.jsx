79a9ae72870e6ca16953b32ee823346014726b6a
Noah Lett <noahblett96@gmail.com>
2024-01-30 07:44:44 -0800

    Created initial search api

---
+++ app/search/page.jsx
@@ -0,0 +1,36 @@
+import getSearchResults from "@/lib/getSearchResults";
+import Card from "../components/Card";
+
+const SearchPage = async ({ searchParams }) => {
+
+  console.log('q is: ', searchParams.q);
+  const results = await getSearchResults(searchParams?.q);
+  console.log(results);
+
+  return (
+    <div className="min-h-screen pt-20">
+      <h1 className="text-3xl font-semibold text-center my-5">Search Results</h1>
+      <div className="flex flex-wrap items-center justify-center">
+      {
+        results && results.length && (
+          results.map((result) => {
+            return (
+              <Card 
+                key={result.id}
+                id={result.id}
+                name={result.name}
+                image_src={result.image_src}
+                author={result.author}
+                ingredients={result.ingredients}
+              />
+            )
+          })
+        )
+      }
+      </div>
+    </div>
+  )
+
+}
+
+export default SearchPage
