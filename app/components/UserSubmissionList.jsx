47673bf77d34af0a9e93cf6eb83b94b0b6d7012a
Noah Lett <noahblett96@gmail.com>
2024-02-13 12:08:27 -0800

    created carousel

--- app/components/UserSubmissionList.jsx
+++ app/components/UserSubmissionList.jsx
@@ -48,15 +48,7 @@ const UserSubmissionList = async () => {
             </div>
         )
     }
-  return (
-    <div className="flex flex-col justify-center items-center p-4 w-full">
-        <h1 className="text-3xl font-semibold">Got a Favorite Recipe?</h1>
-        <div className="bg-white shadow-md mt-4 rounded-md flex flex-col items-center justify-center">
-            <Image src={'/on-phone.svg'} height={300} width={500} priority={true} alt="No Show" />
-            <Link href={'/add-recipe'} className="text-xl font-medium border-[1px] border-white bg-[#8ce5e5] hover:bg-[#58adae] transition-all duration-150 p-2 mb-4 rounded-md m-1">Add it Now!</Link>
-        </div>
-    </div>
-  )
+  return 
 }
 
 export default UserSubmissionList
