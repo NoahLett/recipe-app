2c3af03b814512dfbfe89bea17b1200b7c836f99
Noah Lett <noahblett96@gmail.com>
2024-02-13 13:45:21 -0800

    fixed card image issue

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -19,15 +19,15 @@ const Hero = () => {
   const { data: session } = useSession();
 
     return (
-      <div className='w-full bg-cover bg-center md:min-h-[30rem] py-2 md:py-5 flex flex-col md:justify-around' style={backgroundDiv}>
-      <div className='max-w-[100%] mx-auto grid grid-cols-2'>
-        <div className='flex flex-col justify-center ml-[2rem] md:ml-[7rem] mt-5'>
-          <p className='text-md md:text-2xl text-white font-medium my-1'>EXQUISITE RECIPES</p>
-          <h1 className='text-2xl md:text-4xl lg:text-5xl font-semibold'>{session?.user?.name ? `Hey there, ${session?.user?.name}!` : 'Your Custom Shopping List Is A Tap Away'}</h1>
-          <p className='text-sm md:text-xl text-white font-medium my-2'>Save Some Time. Get Browsin&apos;!</p>
+      <div className='w-full bg-cover bg-center md:min-h-[30rem] py-2 md:py-5 flex flex-col md:justify-around relative' style={backgroundDiv}>
+      <div className='max-w-[55%] md:max-w-[55%] flex'>
+        <div className='flex flex-col justify-center ml-[2rem] md:ml-[5rem] mt-5'>
+          <p className='text-md md:text-2xl lg:text-3xl text-white font-medium my-1'>EXQUISITE RECIPES</p>
+          <h1 className='text-3xl md:text-6xl lg:text-7xl font-semibold'>{session?.user?.name ? `Hey there, ${session?.user?.name}!` : 'Save Time. Eat Food.'}</h1>
+          <p className='text-sm md:text-xl lg:text-2xl text-white font-medium my-2'>All Our Favs. One Place.</p>
         </div>
       </div>
-      <div className='flex flex-col items-center justify-end md:mt-10'>
+      <div className='flex flex-col items-center justify-end mt-3 md:mt-10'>
         <Search />
         <Link className='hover:text-slate-300 transition-all duration-150 my-3 font-medium text-md md:text-lg underline text-white' href='/view-all'>View All</Link>
       </div>
