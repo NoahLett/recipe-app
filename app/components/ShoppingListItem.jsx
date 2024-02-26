8f36c7035f837ddc9312eb4674d759be3ddfa175
Noah Lett <noahblett96@gmail.com>
2024-02-26 12:05:01 -0800

    updated text size of shopping list item

--- app/components/ShoppingListItem.jsx
+++ app/components/ShoppingListItem.jsx
@@ -19,12 +19,12 @@ const ShoppingListItem = ({ ingredient }) => {
       <div className='flex flex-row'>
         <div className='flex flex-row items-center basis-4/5'>
           <CustomCheckbox checked={isChecked} onChange={handleCheckboxChange} />
-          <div className='text-[1.1rem] md:text-xl ml-2'>{ingredient.name}</div>
+          <div className='text-md md:text-xl ml-2 pr-5'>{ingredient.name}</div>
         </div>
         <div className='content-left my-auto ml-1 basis-1/5'>
-          <span className="text-[1.1rem] md:text-xl whitespace-nowrap">{ingredient.quantity}</span>
+          <span className="text-md md:text-xl whitespace-nowrap">{ingredient.quantity}</span>
           {ingredient.measurement !== null && ingredient.measurement !== 'null' && (
-            <span className="text-[1.1rem] md:text-xl ml-3 items-start">{ingredient.measurement}</span>
+            <span className="text-md md:text-xl ml-3 items-start">{ingredient.measurement}</span>
           )}
           {isChecked && <ShoppingListRemoveButton ingredients={ingredient.name} />}
         </div>
